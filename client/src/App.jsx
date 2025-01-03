7f6760f4605053271578e5e6db2b0c92b1c4d93a
samuel maveke <mavekepeter2022@gmail.com>
2025-01-03 15:22:40 +0300

    complete add job ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import Applyjob from './pages/Applyjob'
 import Application from './pages/Application'
 import RecruiterLogin from './components/RecruiterLogin'
 import { AppContext } from './context/Appcontext'
+import Dashboard from './components/Dashboard'
+import AddJob from './pages/AddJob'
+import ManageJobs from './pages/ManageJobs'
+import ViewApplication from './pages/ViewApplication'
+import 'quill/dist/quill.snow.css'
 const App = () => {
   const {showRecruiterLogin} = useContext(AppContext)
   return (
@@ -15,7 +20,11 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/apply-job/:id' element={<Applyjob/>}/>
         <Route path='/applictions' element={<Application/>}/>
-
+        <Route path='/dashboard' element ={<Dashboard/>}>
+        <Route path='add-job' element={<AddJob/>}/>
+        <Route path='manage-jobs' element={<ManageJobs/>}/>
+        <Route path='view-applications' element={<ViewApplication/>}/>
+        </Route>
       </Routes>
     </div>
   )
